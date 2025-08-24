5b777abced2d0f8d78c67f30457cd7efb932b63d
Double-Bee-24 <bodya24102001@gmail.com>
2025-08-24 09:59:52 +0300

    swagger configured

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestiaSwaggerComposer } from '@nestia/sdk';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = await NestiaSwaggerComposer.document(app, {
+    openapi: '3.1',
+    servers: [
+      {
+        url: 'http://localhost:5000',
+        description: 'Localhost',
+      },
+    ],
+  });
+  SwaggerModule.setup('api', app, document as OpenAPIObject);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap().catch((error) => {
-  console.error('Failed to start the application:', error);
+  const logger = new Logger('Bootstrap');
+  logger.error('Failed to start the application:', error);
   process.exit(1);
 });
